9f9e786dde7cca51004b720b3ce890611ff0cd86
Cintia Leal <66495366+CintiaLeal@users.noreply.github.com>
2022-06-17 10:55:18 -0300

    Add files via upload

---
+++ api/src/main/java/com/mantel/api/service/VisualizacionService.java
@@ -0,0 +1,11 @@
+package com.mantel.api.service;
+
+import com.mantel.api.model.Visualizacion;
+
+
+
+import java.util.List;
+public interface VisualizacionService {
+    void agregarVisualizacion(Visualizacion v);
+
+}
