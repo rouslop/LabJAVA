ff0749d07315e69b7f35482bb2ba2e6f7251533e
Cintia Leal <66495366+CintiaLeal@users.noreply.github.com>
2022-06-17 10:56:43 -0300

    Add files via upload

---
+++ api/src/main/java/com/mantel/api/controller/VisualizacionController.java
@@ -0,0 +1,25 @@
+package com.mantel.api.controller;
+import com.mantel.api.model.Usuario;
+import com.mantel.api.model.Visualizacion;
+import com.mantel.api.service.UsuarioService;
+import com.mantel.api.service.VisualizacionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/visualizacion")
+public class VisualizacionController {
+
+    private VisualizacionService visualizacionService;
+
+    public  VisualizacionController (VisualizacionService visualizacionService){
+        super();
+        this.visualizacionService = visualizacionService;
+    }
+
+    @PostMapping("/agregarVisualizacion")
+    public ResponseEntity<String> agregarVisualizacion(@RequestBody Visualizacion visualizacion){
+        visualizacionService.agregarVisualizacion(visualizacion);
+        return new ResponseEntity<String>("Visualizacion creada", HttpStatus.CREATED);
+    }
+}
