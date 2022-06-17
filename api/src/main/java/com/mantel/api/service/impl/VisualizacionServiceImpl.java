e820683af73fb5673f3f235da5a46f4b7cfbd570
Cintia Leal <66495366+CintiaLeal@users.noreply.github.com>
2022-06-17 10:55:42 -0300

    Add files via upload

---
+++ api/src/main/java/com/mantel/api/service/impl/VisualizacionServiceImpl.java
@@ -0,0 +1,23 @@
+package com.mantel.api.service.impl;
+import com.mantel.api.model.Usuario;
+import com.mantel.api.model.Visualizacion;
+import com.mantel.api.service.UsuarioService;
+import com.mantel.api.service.VisualizacionService;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import java.util.List;
+@Service
+@Transactional
+public class VisualizacionServiceImpl implements VisualizacionService {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void agregarVisualizacion(Visualizacion v){
+        em.persist(v);
+    }
+}
