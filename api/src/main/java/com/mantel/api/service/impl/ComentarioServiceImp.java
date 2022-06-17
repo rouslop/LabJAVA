b8df9ac91f68485b9320514bb5dabd093e72cb03
Cintia Leal <66495366+CintiaLeal@users.noreply.github.com>
2022-06-17 10:25:42 -0300

    Add files via upload

---
+++ api/src/main/java/com/mantel/api/service/impl/ComentarioServiceImp.java
@@ -0,0 +1,31 @@
+package com.mantel.api.service.impl;
+
+
+import com.mantel.api.model.Comentario;
+
+import com.mantel.api.model.Usuario;
+import com.mantel.api.service.CategoriaService;
+import com.mantel.api.service.ComentarioService;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class ComentarioServiceImp implements ComentarioService  {
+    @PersistenceContext
+    private EntityManager em;
+    @Override
+    public void agregarComentario(Comentario comentario){
+        em.persist(comentario);
+    }
+
+    public void eliminarComentario(long id){
+        Comentario comentario = em.find(Comentario.class, id);
+        em.remove(comentario);
+    }
+}
